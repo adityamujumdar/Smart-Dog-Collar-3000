5c1986b2cf1739210bfbc97f2ceb3e446b16ce29
Errol Pascua <oniman217@gmail.com>
2021-02-21 12:09:07 -0700

    added highfreq.wav and its functionality

--- test_mic/sound.py
+++ test_mic/sound.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import time
+from playsound import playsound
+
 
 #GPIO SETUP
 channel = 17
@@ -9,8 +11,12 @@ GPIO.setup(channel, GPIO.IN)
 def callback(channel):
 	if GPIO.input(channel):
 		print("Sound Detected!")
-	else:
-		print("Sound Detected!")
+	        playsound('highfreq.wav')
+                time.sleep(1)
+        else:
+		print("Sound Detected!")                
+	        playsound('highfreq.wav')
+                time.sleep(1)
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
 GPIO.add_event_callback(channel, callback)
