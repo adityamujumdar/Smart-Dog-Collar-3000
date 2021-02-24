b68d5c85ee13e5b20d11a826ad0fc179e1c91253
epascua2 <oniman217@gmail.com>
2021-02-23 21:19:21 -0700

    changes

--- test_mic/backup/sound.py
+++ test_mic/backup/sound.py
@@ -1,25 +1,35 @@
+#!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import time
-from playsound import playsound
+import pygame
 
 
 #GPIO SETUP
-channel = 17
+channel = 22
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
+#load wav file
+
+pygame.mixer.init()
+pygame.mixer.music.load("highfreq.wav")
+
 def callback(channel):
-	if GPIO.input(channel):
-		print("Sound Detected!")
-	        playsound('highfreq.wav')
-                time.sleep(1)
-        else:
-		print("Sound Detected!")                
-	        playsound('highfreq.wav')
-                time.sleep(1)
+    if GPIO.input(channel):
+        print("Sound Detected!")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+        time.sleep(1)
+    else:
+        print("Sound Detected!")                 
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+        time.sleep(1)
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
 GPIO.add_event_callback(channel, callback)
 
 while True:
-	time.sleep(1)
+    time.sleep(1)
